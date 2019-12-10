e50d20fbb3aad39b28d17fb3aabeb63325d427b7
mac <machao@hhdata.net.cn>
2019-12-10 18:03:46 +0800

    react

---
+++ build/precache-manifest.91f324de1b176d27d1ef9c188f763ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80da5115d88b85a3ae84d664bfce717",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fce4854118b4a34b9e72",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e26dbf2f04b13308ed08",
+    "url": "./static/js/2.885ab969.chunk.js"
+  },
+  {
+    "revision": "fce4854118b4a34b9e72",
+    "url": "./static/js/main.3e62dfd2.chunk.js"
+  },
+  {
+    "revision": "957320fcd309d4eb732c",
+    "url": "./static/js/runtime-main.91a13098.js"
+  }
+]);
